8f9a3a92ba392cd3ad5abc0574a8226535f8e10f
Raihan-iOS <md.fasi.raihan.ssl@gmail.com>
2025-09-28 02:21:43 +0600

    login n registration API added

---
+++ lib/data/utils/constant.dart
@@ -0,0 +1,5 @@
+class Urls {
+  static const String baseUrl = "http://35.73.30.144:2005/api/v1";
+  static const String login = "${baseUrl}/Login";
+  static const String registration = "${baseUrl}/Registration";
+}
